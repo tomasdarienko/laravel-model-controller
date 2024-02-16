039d95b88a02e28a802dd13996bcf6a75474833e
tomasdarienko <darienkotomas@gmail.com>
2024-02-16 10:54:57 +0100

    fiine esercizio

---
+++ app/Http/Controllers/Guest/MovieController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Movie;
+class MovieController extends Controller
+{
+       public function index(){  
+
+        $movies = Movie::all();
+        return view("welcome" , compact("movies"));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Guest\MovieController as MovieController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class,'index'])->name('homepage') ;
+
+
